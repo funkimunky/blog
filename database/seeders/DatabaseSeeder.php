2fad43b4364cdc9cd044c6b465c47543ca542c6a
David Thomson <davidthomson123@hotmail.com>
2024-05-10 10:37:21 +0100

    Keeping seeding the same as tutorial material

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,35 +16,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $categories = [
-            [
-                'name'=>'Work',
-                'slug'=>'work'
-            ],
-            [
-                'name'=>'Family',
-                'slug'=>'family'
-            ],
-            [
-                'name'=>'Hobbies',
-                'slug'=>'hobbies'
-            ],
-        ];
-        $randomUser = rand(1,5);
-        for($i=0;$i<$randomUser;$i++)
-        {
-            $user = User::factory()->create();
-            foreach($categories as $item)
-            {
-                $category = Category::factory()->create([
-                    'name'=>$item['name'],
-                    'slug'=>$item['slug']
-                ]);
-                Post::factory(rand(1,3))->create([
-                    'user_id'=>$user->id,
-                    'category_id'=>$category->id
-                ]);
-            }
-        };
+
+
+            $user = User::factory()->create([
+                'username'=>'John Doe'
+            ]);
+
+            Post::factory(5)->create([
+                'user_id'=>$user->id
+            ]);
+
+
     }
 }
