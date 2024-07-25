e851e81aa8c63ea4df488ff43f88097ab1ba0edf
David Thomson <davidthomson123@hotmail.com>
2024-07-25 14:32:04 +0100

    updating post page

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,57 +1,27 @@
 <x-layout>
-{{--    <article>--}}
-{{--        <h1>{{ $post->title }}</h1>--}}
-{{--        <p>By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a></p>--}}
-{{--        <div>{!! $post->body !!}</div>--}}
-{{--    </article>--}}
-{{--    <a href="/">Go Back</a>--}}
 
-    <!doctype html>
-
-    <title>Laravel From Scratch Blog</title>
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <body style="font-family: Open Sans, sans-serif">
     <section class="px-6 py-8">
-        <nav class="md:flex md:justify-between md:items-center">
-            <div>
-                <a href="/">
-                    <img src="./images/logo.svg" alt="Laracasts Logo" width="165" height="16">
-                </a>
-            </div>
-
-            <div class="mt-8 md:mt-0">
-                <a href="/" class="text-xs font-bold uppercase">Home Page</a>
-
-                <a href="#"
-                   class="transition-colors duration-300 bg-blue-500 hover:bg-blue-600 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">Subscribe
-                    for Updates</a>
-            </div>
-        </nav>
 
         <main class="max-w-6xl mx-auto mt-10 lg:mt-20 space-y-6">
             <article class="max-w-4xl mx-auto lg:grid lg:grid-cols-12 gap-x-10">
                 <div class="col-span-4 lg:text-center lg:pt-14 mb-10">
-                    <img src="./images/illustration-1.png" alt="" class="rounded-xl">
+                    <img src="/images/illustration-1.png" alt="" class="rounded-xl">
 
                     <p class="mt-4 block text-gray-400 text-xs">
-                        Published <time>1 day ago</time>
+                        Published <time>{{$post->created_at->diffForHumans()}}</time>
                     </p>
 
                     <div class="flex items-center lg:justify-center text-sm mt-4">
-                        <img src="./images/lary-avatar.svg" alt="Lary avatar">
+                        <img src="/images/lary-avatar.svg" alt="Lary avatar">
                         <div class="ml-3 text-left">
-                            <h5 class="font-bold">Lary Laracore</h5>
-                            <h6>Mascot at Laracasts</h6>
+                            <h5 class="font-bold">{{$post->author->name}}</h5>
                         </div>
                     </div>
                 </div>
 
                 <div class="col-span-8">
                     <div class="hidden lg:flex justify-between mb-6">
-                        <a href="#"
+                        <a href="/"
                            class="transition-colors duration-300 relative inline-flex items-center text-lg hover:text-blue-500">
                             <svg width="22" height="22" viewBox="0 0 22 22" class="mr-2">
                                 <g fill="none" fill-rule="evenodd">
@@ -67,9 +37,9 @@
                         </a>
 
                         <div class="space-x-2">
-                            <a href="#"
+                            <a href="/categories/{{$post->category->slug}}"
                                class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                               style="font-size: 10px">Techniques</a>
+                               style="font-size: 10px">{{$post->category->name}}</a>
                             <a href="#"
                                class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold"
                                style="font-size: 10px">Updates</a>
@@ -128,33 +98,5 @@
                 </div>
             </article>
         </main>
-
-        <footer class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
-            <img src="./images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
-            <h5 class="text-3xl">Stay in touch with the latest posts</h5>
-            <p class="text-sm">Promise to keep the inbox clean. No bugs.</p>
-
-            <div class="mt-10">
-                <div class="relative inline-block mx-auto lg:bg-gray-200 rounded-full">
-                    <form method="POST" action="#" class="lg:flex text-sm">
-                        <div class="lg:py-3 lg:px-5 flex items-center">
-                            <label for="email" class="hidden lg:inline-block">
-                                <img src="./images/mailbox-icon.svg" alt="mailbox letter">
-                            </label>
-
-                            <input id="email" type="text" placeholder="Your email address"
-                                   class="lg:bg-transparent pl-4 focus-within:outline-none">
-                        </div>
-
-                        <button type="submit"
-                                class="transition-colors duration-300 bg-blue-500 hover:bg-blue-600 mt-4 lg:mt-0 lg:ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-8">
-                            Subscribe
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </footer>
     </section>
-    </body>
-
 </x-layout>
